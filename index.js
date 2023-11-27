4e042d2018f0552223b3d06ee9111053eacd8066
unknown <you@example.com>
2023-11-27 10:28:16 +0600

    77.2

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
 
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartCollection = client.db("bistroDb").collection("carts");
     
     app.get('/menu', async(req, res) => {
       const result = await menuCollection.find().toArray()
@@ -35,7 +36,15 @@ async function run() {
     app.get('/reviews', async(req, res) => {
       const result = await reviewCollection.find().toArray()
       res.send(result);
-    })
+    });
+
+    // Cart collection
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
